d9c1e1e66602a3cf75dff3bb501323657e8996db
Infrabolt <j.m.c.mateus300@gmail.com>
2025-04-28 17:41:13 -0300

    molde banco de dados

--- BancoDeDados.py
+++ BancoDeDados.py
@@ -1,1 +1,10 @@
+import sqlite3
+
+banco = sqlite3.connect("Artigos_HCufpe.db")
+
+cursor = banco.cursor()
+
+cursor.execute()
+
+cursor.execute("CREATE TABLE Artigos(Titulo text, Autores text, Resumo text, Data de publicação integer, link de acesso text)")
 
