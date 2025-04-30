f397d1832267c09d2aacdd01c4fe2d3f9b439b2f
Infrabolt <j.m.c.mateus300@gmail.com>
2025-04-30 15:48:40 -0300

    Criando Funções 0.1

--- BancoDeDados.py
+++ BancoDeDados.py
@@ -1,15 +1,19 @@
 import sqlite3
 
-banco = sqlite3.connect("Artigos_HCufpe.db")
+def CriarBanco():
+    banco = sqlite3.connect("Artigos_HCufpe.db")
 
-cursor = banco.cursor()
+    cursor = banco.cursor()
 
-cursor.execute("SELECT 1")  # só testando a conexão
+    cursor.execute("SELECT 1")  # só testando a conexão
 
-cursor.execute("CREATE TABLE Artigos(Titulo text, Autores text, Resumo text, Data de publicação integer, link de acesso text)")
+    cursor.execute("CREATE TABLE Artigos(Titulo text, Autores text, Resumo text, Data de publicação integer, link de acesso text)")
 
-# excluir duplicatas
-cursor.execute("DELETE FROM Artigos WHERE rowid NOT IN (SELECT MIN(rowid) FROM Artigos GROUP BY Titulo, Autores, Resumo, [Data de publicação], [link de acesso])")
+    # excluir duplicatas
+    cursor.execute("DELETE FROM Artigos WHERE rowid NOT IN (SELECT MIN(rowid) FROM Artigos GROUP BY Titulo, Autores, Resumo, [Data de publicação], [link de acesso])")
 
-banco.commit()
-banco.close()
+    banco.commit()
+    banco.close()
+
+def salvar_artigo(titulo, autores, resumo, data_publicacao, link):
+    return
