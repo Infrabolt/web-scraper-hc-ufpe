e9d2d9c103a913eddc03de174400bb04f64386ee
Infrabolt <j.m.c.mateus300@gmail.com>
2025-04-30 15:58:47 -0300

    WebScrwler 0.3

--- WebScrawler.py
+++ WebScrawler.py
@@ -46,13 +46,18 @@ def processar_artigos_e_salvar(termos_de_busca):
         print(f"🔍 Buscando artigos para: {termo}")
         artigos = buscar_artigos_google_academico(termo)
         for artigo in artigos:
-            salvar_artigo(
-                titulo=artigo["titulo"],
-                autores=artigo["autores"],
-                resumo=artigo["resumo"],
-                data_publicacao=artigo["data_publicacao"],
-                link=artigo["link"]
-            )
+            # salvar_artigo(
+            #     titulo=artigo["titulo"],
+            #     autores=artigo["autores"],
+            #     resumo=artigo["resumo"],
+            #     data_publicacao=artigo["data_publicacao"],
+            #     link=artigo["link"]
+            # )
+            print(artigo["titulo"] + "\n",
+                  artigo["autores"],"\n",
+                  artigo["link"]
+                  )
+    
 
 # Exemplo de termos
 termos = [
