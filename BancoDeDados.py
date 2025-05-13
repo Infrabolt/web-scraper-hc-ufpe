fd2f9b5f73a46fdc328b69164ff39861c7e32517
b1elfreytas <gabrielfreytas2004@gmail.com>
2025-05-13 15:38:58 -0300

    Banco de Dados atualizado

--- BancoDeDados.py
+++ BancoDeDados.py
@@ -1,23 +1,23 @@
 
 import pandas as pd
 
+def salvar_artigo(titulo, autores, resumo, data_publicacao, link):
+    # Exemplo de lista com os dados coletados
+    Artigos = [
+        {
+            "titulo": titulo,
+            "autores": autores,
+            "resumo": resumo,
+            "data_publicacao": data_publicacao,
+            "link": link
+        }
+        
+    ]
+    # Cria o DataFrame
+    df = pd.DataFrame(Artigos)
 
-# Exemplo de lista com os dados coletados
-Artigos = [
-    {
-        "titulo": "titulo",
-        "autores": "autores",
-        "resumo": "resumo",
-        "data_publicacao": "data_publicacao",
-        "link": "link"
-    }
-    
-]
-# Cria o DataFrame
-df = pd.DataFrame(Artigos)
-
-# Exporta para Excel
-df.to_excel("Artigos.xlsx", index=False)
+    # Exporta para Excel
+    df.to_excel("Artigos.xlsx", index=False)
 
 
 
